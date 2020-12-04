c288f844e26617694fde0e9359f75d00b90ca9e8
Sooraj Kumar <the.wistler@gmail.com>
2020-12-04 11:54:07 -0500

    Cleanup/Refactoring Day 4 solution - using regexp - added validation error info - minor cleanup

--- day04/day.go
+++ day04/day.go
@@ -3,90 +3,57 @@ package day04
 import (
 	"fmt"
 	"log"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
-func checkPassportField(key string, value string) bool {
+func checkPassportField(key string, value string) (bool, error) {
 	switch key {
 	case "byr":
-		v, err := strconv.Atoi(value)
-		if err != nil {
-			return false
-		}
-		if v < 1920 || v > 2002 {
-			return false
+		if v, err := strconv.Atoi(value); err != nil || v < 1920 || v > 2002 {
+			return false, fmt.Errorf("byr %q", value)
 		}
 	case "iyr":
-		v, err := strconv.Atoi(value)
-		if err != nil {
-			return false
-		}
-		if v < 2010 || v > 2020 {
-			return false
+		if v, err := strconv.Atoi(value); err != nil || v < 2010 || v > 2020 {
+			return false, fmt.Errorf("iyr %q", value)
 		}
 	case "eyr":
-		v, err := strconv.Atoi(value)
-		if err != nil {
-			return false
-		}
-		if v < 2020 || v > 2030 {
-			return false
+		if v, err := strconv.Atoi(value); err != nil || v < 2020 || v > 2030 {
+			return false, fmt.Errorf("eyr %q", value)
 		}
 	case "hgt":
-		if strings.HasSuffix(value, "cm") {
-			v := 0
-			n, err := fmt.Sscanf(value, "%dcm", &v)
-			if err != nil {
-				return false
-			}
-			if n != 1 {
-				return false
-			}
+		v := 0
+		if n, err := fmt.Sscanf(value, "%dcm", &v); err == nil && n == 1 {
 			if v < 150 || v > 193 {
-				return false
+				return false, fmt.Errorf("hgt cm %q", value)
 			}
-		} else if strings.HasSuffix(value, "in") {
-			v := 0
-			n, err := fmt.Sscanf(value, "%din", &v)
-			if err != nil {
-				return false
-			}
-			if n != 1 {
-				return false
-			}
+		} else if n, err := fmt.Sscanf(value, "%din", &v); err == nil && n == 1 {
 			if v < 59 || v > 76 {
-				return false
+				return false, fmt.Errorf("hgt in %q", value)
 			}
 		} else {
-			return false
+			return false, fmt.Errorf("hgt %q", value)
 		}
 	case "hcl":
-		if !strings.HasPrefix(value, "#") || strings.ToLower(value) != value {
-			return false
-		}
-		_, err := strconv.ParseInt(value[1:], 16, 0)
-		if err != nil {
-			return false
+		if matched, err := regexp.MatchString("^#[0-9a-f]{6}$", value); err != nil || !matched {
+			return false, fmt.Errorf("hcl %q", value)
 		}
 	case "ecl":
-		if value != "amb" && value != "blu" && value != "brn" && value != "gry" && value != "grn" && value != "hzl" && value != "oth" {
-			return false
+		if matched, err := regexp.MatchString("^(amb|blu|brn|gry|grn|hzl|oth)$", value); err != nil || !matched {
+			return false, fmt.Errorf("ecl %q", value)
 		}
 	case "pid":
-		if len(value) != 9 {
-			return false
-		}
-		for _, ch := range value {
-			if !strings.ContainsAny(string(ch), "1234567890") {
-				return false
-			}
+		if matched, err := regexp.MatchString("^[0-9]{9}$", value); err != nil || !matched {
+			return false, fmt.Errorf("pid %q", value)
 		}
 	case "cid":
 		// do nothing
+	default:
+		return false, fmt.Errorf("Unknown field: %q", key)
 	}
 
-	return true
+	return true, nil
 }
 
 func checkPassport(keyValuesPairs []string, validateFieldContent bool) bool {
@@ -115,8 +82,11 @@ func checkPassport(keyValuesPairs []string, validateFieldContent bool) bool {
 		if !found {
 			panic(fmt.Sprintf("Unknown field: %q", key))
 		}
-		if validateFieldContent && checkPassportField(key, value) == false {
-			return false
+		if validateFieldContent {
+			if valid, _ := checkPassportField(key, value); !valid {
+				// log.Printf("Field validation error: %s", err)
+				return false
+			}
 		}
 	}
 
@@ -137,43 +107,53 @@ func checkPassport(keyValuesPairs []string, validateFieldContent bool) bool {
 	return true
 }
 
-func groupIntoPassportsAndValidate(input []string, validateFields bool) int {
-	validPassports := 0
-	passport := []string{}
+// a passport is grouped into a set of "key:value" strings
+func getPassports(input []string) [][]string {
+	passports := [][]string{}
+	current := []string{}
 	for _, line := range input {
 		if strings.Trim(line, " ") == "" {
-			if checkPassport(passport, validateFields) {
-				validPassports++
-			}
-			passport = []string{}
+			passports = append(passports, current)
+			current = []string{}
 		} else {
-			passport = append(passport, strings.Split(line, " ")...)
+			current = append(current, strings.Split(line, " ")...)
 		}
 	}
-	if len(passport) != 0 {
-		if checkPassport(passport, validateFields) {
-			validPassports++
-		}
-		passport = []string{}
+	if len(current) != 0 {
+		passports = append(passports, current)
+		current = []string{}
 	}
-
-	return validPassports
+	return passports
 }
 
 func part1(input []string) int {
 	log.SetPrefix("Day 4: Part 1: ")
 	log.SetFlags(0)
 
-	ans := groupIntoPassportsAndValidate(input, false)
-	log.Printf("Answer: %v", ans)
-	return ans
+	validPassports := 0
+	passports := getPassports(input)
+	for _, passport := range passports {
+		if checkPassport(passport, false) {
+			validPassports++
+		}
+	}
+
+	log.Printf("Answer: %v", validPassports)
+	return validPassports
 }
 
 func part2(input []string) int {
 	log.SetPrefix("Day 4: Part 2: ")
 	log.SetFlags(0)
 
-	ans := groupIntoPassportsAndValidate(input, true)
-	log.Printf("Answer: %v", ans)
-	return ans
+	validPassports := 0
+	passports := getPassports(input)
+	for _, passport := range passports {
+		if checkPassport(passport, true) {
+			validPassports++
+		}
+	}
+
+	log.Printf("Answer: %v", validPassports)
+	return validPassports
 }
